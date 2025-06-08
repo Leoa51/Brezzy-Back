c1fa94fc44143692f23037d3c417c485b349f9f4
Tom  Raineri <tomstory9@gmail.com>
2025-06-08 21:09:46 +0200

    fix: fix language

--- src/models/conversation.model.js
+++ src/models/conversation.model.js
@@ -1,11 +1,9 @@
-// Import de la bibliothèque Mongoose pour interagir avec MongoDB
 const mongoose = require('mongoose');
 
-// Définition d'un schéma pour une collection de conversations
 const conversationSchema = new mongoose.Schema({
     participants: [{
         type: mongoose.Schema.Types.ObjectId,
-        ref: 'User',        // Référence vers les utilisateurs participants
+        ref: 'User',
         required: true
     }],
     messages: [{
@@ -20,16 +18,16 @@ const conversationSchema = new mongoose.Schema({
             trim: true,
             maxlength: 1000
         },
-        date_message: {
+        sentAt: {
             type: Date,
             default: Date.now
         },
-        picture: {
-            type: String,   // URL ou chemin vers l'image
+        pictureUrl: {
+            type: String,
             default: ''
         },
-        video: {
-            type: String,   // URL ou chemin vers la vidéo
+        videoUrl: {
+            type: String,
             default: ''
         },
         isRead: {
@@ -37,7 +35,7 @@ const conversationSchema = new mongoose.Schema({
             default: false
         }
     }],
-    lastMessage: {
+    lastMessageAt: {
         type: Date,
         default: Date.now
     },
@@ -47,20 +45,15 @@ const conversationSchema = new mongoose.Schema({
     }
 });
 
-// Middleware pour mettre à jour lastMessage automatiquement
-conversationSchema.pre('save', function(next) {
+conversationSchema.pre('save', function (next) {
     if (this.messages && this.messages.length > 0) {
-        this.lastMessage = this.messages[this.messages.length - 1].date_message;
+        this.lastMessageAt = this.messages[this.messages.length - 1].sentAt;
     }
     next();
 });
 
-// Index pour optimiser les recherches par participants
 conversationSchema.index({ participants: 1 });
-conversationSchema.index({ lastMessage: -1 });
+conversationSchema.index({ lastMessageAt: -1 });
 
-// Création d'un modèle basé sur le schéma
 const Conversation = mongoose.model('Conversation', conversationSchema);
-
-// Exporte le modèle Conversation pour qu'il soit utilisé dans d'autres fichiers
 module.exports = Conversation;
