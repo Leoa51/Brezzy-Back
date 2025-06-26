fdddd07920e63f3b20dc9cf674546924d35abfbf
Tom Raineri <tomstory9@gmail.com>
2025-06-26 10:56:41 +0200

    wip prod ready

--- src/index.js
+++ src/index.js
@@ -30,7 +30,7 @@ switch (process.env.SERVICE) {
   case 'users':
     app.use('/', authMiddleware, usersRouter);
   case 'posts':
-    app.use('/', authMiddleware, postsRouter);
+    app.use('/api/posts', authMiddleware, postsRouter);
   case 'chat':
     app.use('/', authMiddleware, conversationRouter);
   case 'auth':
