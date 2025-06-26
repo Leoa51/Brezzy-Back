e92df6ce1bef7589f8a7c9bad842a56cf0b5ff5e
Tom Raineri <tomstory9@gmail.com>
2025-06-26 10:24:42 +0200

    delete console log

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1064,7 +1064,6 @@ export async function getMe(req, res) {
     if (!errors.isEmpty()) {
         return res.status(422).json({ errors: errors.array() });
     }
-    console.log(req, req.user)
     try {
         const user = await prisma.user_.findUnique({
             where: { id: req.user.id },
